5d977c5eaffea09b517b7131ff35c8e03ad9688c
sunil1206 <masunil12@gmail.com>
2025-05-28 17:55:54 +0200

    Update urls.py

--- portfolioproject/spamdetection/urls.py
+++ portfolioproject/spamdetection/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('spam/', views.predict_spam, name='predict_spam'),
-    path('predict-spam/', views.predict_spam, name='predict_spam'),
+    # path('predict-spam/', views.predict_spam, name='predict_spam'),
+    path('spam/', views.predict_spam, name='predict_spam'),
 ]
